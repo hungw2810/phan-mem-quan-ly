d949d3a199f8922efb17c0098e32f3f024af257f
DuongwLe <lthdng2207@gmail.com>
2021-06-22 23:19:12 +0700

    hung ga

--- Main.cs
+++ Main.cs
@@ -16,5 +16,10 @@ namespace Quanly
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
