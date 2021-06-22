e3b73354d06d1777610600dad9f63cd15770dc8b
hungw2810 <hungnn2810@gmail.com>
2021-06-22 23:12:09 +0700

    3

--- Main.cs
+++ Main.cs
@@ -16,15 +16,5 @@ namespace Quanly
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
     }
 }
