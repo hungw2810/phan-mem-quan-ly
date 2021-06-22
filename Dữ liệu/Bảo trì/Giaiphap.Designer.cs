b29b72928949df593bea1ad348d0538b581b48c6
thanhhvnh01 <duythanh.qn1@gmail.com>
2021-06-23 01:15:10 +0700

    remastered

--- Dữ liệu/Bảo trì/Giaiphap.Designer.cs
+++ Dữ liệu/Bảo trì/Giaiphap.Designer.cs
@@ -29,12 +29,127 @@ namespace Quanly.Dữ_liệu.Bảo_trì
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtGiaiphap = new System.Windows.Forms.RichTextBox();
+            this.txtChiPhi = new System.Windows.Forms.TextBox();
+            this.txtThoigian = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 212);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(846, 248);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtGiaiphap
+            // 
+            this.txtGiaiphap.Location = new System.Drawing.Point(338, 42);
+            this.txtGiaiphap.Name = "txtGiaiphap";
+            this.txtGiaiphap.Size = new System.Drawing.Size(206, 40);
+            this.txtGiaiphap.TabIndex = 2;
+            this.txtGiaiphap.Text = "";
+            // 
+            // txtChiPhi
+            // 
+            this.txtChiPhi.Location = new System.Drawing.Point(338, 109);
+            this.txtChiPhi.Name = "txtChiPhi";
+            this.txtChiPhi.Size = new System.Drawing.Size(206, 22);
+            this.txtChiPhi.TabIndex = 3;
+            // 
+            // txtThoigian
+            // 
+            this.txtThoigian.Location = new System.Drawing.Point(338, 155);
+            this.txtThoigian.Name = "txtThoigian";
+            this.txtThoigian.Size = new System.Drawing.Size(206, 22);
+            this.txtThoigian.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(215, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tên Giải Pháp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(262, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Chi Phí";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(244, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Thời Gian";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(56, 479);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(89, 33);
+            this.btnThem.TabIndex = 5;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(201, 479);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Giaiphap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(904, 524);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtThoigian);
+            this.Controls.Add(this.txtChiPhi);
+            this.Controls.Add(this.txtGiaiphap);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Giaiphap";
             this.Text = "Giaiphap";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox txtGiaiphap;
+        private System.Windows.Forms.TextBox txtChiPhi;
+        private System.Windows.Forms.TextBox txtThoigian;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button button2;
     }
 }
