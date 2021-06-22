96f0866497ee194ce5fc70a71ba1515fccc32952
hungw2810 <hungnn2810@gmail.com>
2021-06-23 01:13:42 +0700

    event menu

---
+++ Dữ liệu/Maytinh.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quanly.Dữ_liệu
+{
+    public partial class Maytinh : Form
+    {
+        public Maytinh()
+        {
+            InitializeComponent();
+        }
+    }
+}
