d7076c1f434a5b8da15cafaea0c8114e863963e5
Mạnh Duy <bobifish20@gmail.com>
2021-06-23 12:45:15 +0700

    Dữ liệu Nguyên nhân, Nhà bảo trì (done)

--- Dữ liệu/Bảo trì/Nhabaotri.Designer.cs
+++ Dữ liệu/Bảo trì/Nhabaotri.Designer.cs
@@ -29,12 +29,221 @@ namespace Quanly.Dữ_liệu.Bảo_trì
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblManhabaotri = new System.Windows.Forms.Label();
+            this.lblTennhabaotri = new System.Windows.Forms.Label();
+            this.lblDienthoai = new System.Windows.Forms.Label();
+            this.lblDiachi = new System.Windows.Forms.Label();
+            this.txtManhabaotri = new System.Windows.Forms.TextBox();
+            this.txtTennhabaotri = new System.Windows.Forms.TextBox();
+            this.txtDiachi = new System.Windows.Forms.TextBox();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnBoqua = new System.Windows.Forms.Button();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.mskDienthoai = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblManhabaotri
+            // 
+            this.lblManhabaotri.AutoSize = true;
+            this.lblManhabaotri.Location = new System.Drawing.Point(96, 118);
+            this.lblManhabaotri.Name = "lblManhabaotri";
+            this.lblManhabaotri.Size = new System.Drawing.Size(99, 17);
+            this.lblManhabaotri.TabIndex = 0;
+            this.lblManhabaotri.Text = "Mã nhà bảo trì";
+            // 
+            // lblTennhabaotri
+            // 
+            this.lblTennhabaotri.AutoSize = true;
+            this.lblTennhabaotri.Location = new System.Drawing.Point(96, 160);
+            this.lblTennhabaotri.Name = "lblTennhabaotri";
+            this.lblTennhabaotri.Size = new System.Drawing.Size(105, 17);
+            this.lblTennhabaotri.TabIndex = 1;
+            this.lblTennhabaotri.Text = "Tên nhà bảo trì";
+            // 
+            // lblDienthoai
+            // 
+            this.lblDienthoai.AutoSize = true;
+            this.lblDienthoai.Location = new System.Drawing.Point(432, 164);
+            this.lblDienthoai.Name = "lblDienthoai";
+            this.lblDienthoai.Size = new System.Drawing.Size(72, 17);
+            this.lblDienthoai.TabIndex = 2;
+            this.lblDienthoai.Text = "Điện thoại";
+            // 
+            // lblDiachi
+            // 
+            this.lblDiachi.AutoSize = true;
+            this.lblDiachi.Location = new System.Drawing.Point(432, 116);
+            this.lblDiachi.Name = "lblDiachi";
+            this.lblDiachi.Size = new System.Drawing.Size(51, 17);
+            this.lblDiachi.TabIndex = 3;
+            this.lblDiachi.Text = "Địa chỉ";
+            // 
+            // txtManhabaotri
+            // 
+            this.txtManhabaotri.Location = new System.Drawing.Point(235, 118);
+            this.txtManhabaotri.Name = "txtManhabaotri";
+            this.txtManhabaotri.Size = new System.Drawing.Size(100, 22);
+            this.txtManhabaotri.TabIndex = 4;
+            // 
+            // txtTennhabaotri
+            // 
+            this.txtTennhabaotri.Location = new System.Drawing.Point(235, 160);
+            this.txtTennhabaotri.Name = "txtTennhabaotri";
+            this.txtTennhabaotri.Size = new System.Drawing.Size(100, 22);
+            this.txtTennhabaotri.TabIndex = 5;
+            // 
+            // txtDiachi
+            // 
+            this.txtDiachi.Location = new System.Drawing.Point(525, 111);
+            this.txtDiachi.Name = "txtDiachi";
+            this.txtDiachi.Size = new System.Drawing.Size(100, 22);
+            this.txtDiachi.TabIndex = 7;
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(99, 236);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.RowHeadersWidth = 51;
+            this.DataGridView.RowTemplate.Height = 24;
+            this.DataGridView.Size = new System.Drawing.Size(550, 150);
+            this.DataGridView.TabIndex = 8;
+            this.DataGridView.Click += new System.EventHandler(this.DataGridView_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(118, 431);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(62, 29);
+            this.btnThem.TabIndex = 9;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(210, 431);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(62, 29);
+            this.btnSua.TabIndex = 11;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(303, 431);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(62, 29);
+            this.btnLuu.TabIndex = 12;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(397, 431);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(62, 29);
+            this.btnXoa.TabIndex = 13;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnBoqua
+            // 
+            this.btnBoqua.Location = new System.Drawing.Point(484, 431);
+            this.btnBoqua.Name = "btnBoqua";
+            this.btnBoqua.Size = new System.Drawing.Size(78, 29);
+            this.btnBoqua.TabIndex = 14;
+            this.btnBoqua.Text = "Bỏ qua";
+            this.btnBoqua.UseVisualStyleBackColor = true;
+            this.btnBoqua.Click += new System.EventHandler(this.btnBoqua_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(577, 431);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(62, 29);
+            this.btnDong.TabIndex = 15;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // mskDienthoai
+            // 
+            this.mskDienthoai.Location = new System.Drawing.Point(525, 164);
+            this.mskDienthoai.Mask = "0000-9999";
+            this.mskDienthoai.Name = "mskDienthoai";
+            this.mskDienthoai.Size = new System.Drawing.Size(100, 22);
+            this.mskDienthoai.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(172, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 36);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "DANH SÁCH NHÀ BẢO TRÌ";
+            // 
+            // Nhabaotri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(741, 493);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mskDienthoai);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnBoqua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.txtDiachi);
+            this.Controls.Add(this.txtTennhabaotri);
+            this.Controls.Add(this.txtManhabaotri);
+            this.Controls.Add(this.lblDiachi);
+            this.Controls.Add(this.lblDienthoai);
+            this.Controls.Add(this.lblTennhabaotri);
+            this.Controls.Add(this.lblManhabaotri);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Nhabaotri";
             this.Text = "Nhabaotrics";
+            this.Load += new System.EventHandler(this.Nhabaotri_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblManhabaotri;
+        private System.Windows.Forms.Label lblTennhabaotri;
+        private System.Windows.Forms.Label lblDienthoai;
+        private System.Windows.Forms.Label lblDiachi;
+        private System.Windows.Forms.TextBox txtManhabaotri;
+        private System.Windows.Forms.TextBox txtTennhabaotri;
+        private System.Windows.Forms.TextBox txtDiachi;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnBoqua;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.MaskedTextBox mskDienthoai;
+        private System.Windows.Forms.Label label1;
     }
 }
