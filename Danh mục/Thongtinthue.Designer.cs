d69e7be074fd26901fef762f4c3f67cd66a8242e
hungw2810 <hungnn2810@gmail.com>
2021-06-24 11:11:30 +0700

    test form thong tin thue

--- Danh mục/Thongtinthue.Designer.cs
+++ Danh mục/Thongtinthue.Designer.cs
@@ -29,12 +29,100 @@ namespace Quanly.Danh_mục
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtHotenkhach = new System.Windows.Forms.TextBox();
+            this.cboNhanvien = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHotenkhach
+            // 
+            this.txtHotenkhach.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHotenkhach.Location = new System.Drawing.Point(151, 37);
+            this.txtHotenkhach.Name = "txtHotenkhach";
+            this.txtHotenkhach.Size = new System.Drawing.Size(137, 26);
+            this.txtHotenkhach.TabIndex = 0;
+            // 
+            // cboNhanvien
+            // 
+            this.cboNhanvien.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboNhanvien.FormattingEnabled = true;
+            this.cboNhanvien.Location = new System.Drawing.Point(151, 89);
+            this.cboNhanvien.Name = "cboNhanvien";
+            this.cboNhanvien.Size = new System.Drawing.Size(137, 27);
+            this.cboNhanvien.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(151, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.ForeColor = System.Drawing.Color.Red;
+            this.btnHuy.Location = new System.Drawing.Point(231, 146);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(57, 32);
+            this.btnHuy.TabIndex = 3;
+            this.btnHuy.Text = "Huỷ";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Họ tên khách";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nhân viên";
+            // 
+            // Thongtinthue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Thongtinthue";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(335, 196);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cboNhanvien);
+            this.Controls.Add(this.txtHotenkhach);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Thongtinthue";
+            this.Text = "Thông tin thuê";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtHotenkhach;
+        private System.Windows.Forms.ComboBox cboNhanvien;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
