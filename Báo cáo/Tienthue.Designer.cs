69b378cd4826f0c396cb018b84f27baeca290054
thanhhvnh01 <duythanh.qn1@gmail.com>
2021-06-24 16:01:47 +0700

    form TienThue (chua xong)

--- Báo cáo/Tienthue.Designer.cs
+++ Báo cáo/Tienthue.Designer.cs
@@ -29,12 +29,130 @@ namespace Quanly.Báo_cáo
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.txtThang = new System.Windows.Forms.TextBox();
+            this.txtNam = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.txtTongTien = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 159);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(986, 334);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(648, 31);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(82, 51);
+            this.btnTim.TabIndex = 4;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // txtThang
+            // 
+            this.txtThang.Location = new System.Drawing.Point(93, 26);
+            this.txtThang.Name = "txtThang";
+            this.txtThang.Size = new System.Drawing.Size(55, 22);
+            this.txtThang.TabIndex = 3;
+            // 
+            // txtNam
+            // 
+            this.txtNam.Location = new System.Drawing.Point(233, 26);
+            this.txtNam.Name = "txtNam";
+            this.txtNam.Size = new System.Drawing.Size(61, 22);
+            this.txtNam.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tháng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Năm";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(761, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(237, 95);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // txtTongTien
+            // 
+            this.txtTongTien.Location = new System.Drawing.Point(104, 114);
+            this.txtTongTien.Name = "txtTongTien";
+            this.txtTongTien.Size = new System.Drawing.Size(129, 22);
+            this.txtTongTien.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tổng Tiền:";
+            // 
+            // Tienthue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1010, 505);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTongTien);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.txtNam);
+            this.Controls.Add(this.txtThang);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Tienthue";
             this.Text = "Tienthue";
+            this.Load += new System.EventHandler(this.Tienthue_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtThang;
+        private System.Windows.Forms.TextBox txtNam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox txtTongTien;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Báo cáo/Tienthue.cs
+++ Báo cáo/Tienthue.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Quanly.Báo_cáo
 {
     public partial class Tienthue : Form
     {
+        DataTable TT,XX;
         public Tienthue()
         {
             InitializeComponent();
         }
+
+       
+
+        private void Tienthue_Load(object sender, EventArgs e)
+        {
+            dataGridView2.Visible = false;
+        }
+        private void Load_DataGridView()
+        {
+           
+            
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.EditMode = DataGridViewEditMode.EditProgrammatically;
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            string tt;
+            tt = "SELECT SUM(TongTien)  FROM ThueMay";
+            XX = Functions.GetDataToTable(tt);
+            
+            dataGridView2.DataSource = XX;
+            if (txtThang.Text == "")
+            {
+                MessageBox.Show("Bạn phải nhập tháng", "Thông Báo", MessageBoxButtons.OK);
+            }
+            if (txtNam.Text == "")
+            {
+                MessageBox.Show("Bạn phải nhập năm", "Thông Báo", MessageBoxButtons.OK);
+            }
+            string sql;
+            sql = "Select * from ThueMay Where 1=1"; dataGridView1.DataSource = TT; 
+            TT = Functions.GetDataToTable(sql);
+            if (txtThang.Text != "")
+            {
+                sql = sql + "AND MONTH(NgayThue) =" + txtThang.Text;    
+            }
+            if (txtNam.Text != "")
+                sql = sql + "AND YEAR(NgayThue) =" + txtNam.Text;
+             txtTongTien.Text=dataGridView2.CurrentRow.Cells[0].Value.ToString();
+            Load_DataGridView();
+        }
     }
 }
+    
