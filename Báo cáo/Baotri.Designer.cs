5815b4f41ea5dc2a4bf62f6e62e9d363d6c36253
hungw2810 <hungnn2810@gmail.com>
2021-06-25 18:44:00 +0700

    form bao cao ( còn phần xuất excel) dương làm nốt

--- Báo cáo/Baotri.Designer.cs
+++ Báo cáo/Baotri.Designer.cs
@@ -103,7 +103,7 @@ namespace Quanly.Báo_cáo
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(46, 70);
+            this.label2.Location = new System.Drawing.Point(41, 70);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 17);
--- Báo cáo/Baotri.cs
+++ Báo cáo/Baotri.cs
@@ -22,6 +22,9 @@ namespace Quanly.Báo_cáo
             cboThang.SelectedIndex = -1;
             cboQuy.SelectedIndex = -1;
             txtNam.Text = "";
+            cboThang.Enabled = true;
+            cboQuy.Enabled = true;
+            txtNam.Enabled = true;
         }
 
         private void Baotri_Load(object sender, EventArgs e)
@@ -47,22 +50,67 @@ namespace Quanly.Báo_cáo
                 MessageBox.Show("Hãy nhập một điều kiện tìm kiếm!!!", "Yêu cầu ...", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-            sql = "SELECT b.id_phong,c.TongChiPhi FROM CTBT a INNER JOIN MayTinh b ON a.id_may=b.id_may INNER JOIN BaoTri c ON a.id_baotri=c.id_baotri WHERE 1=1";
-            if (cboThang.Text != "")
-                sql = sql + " AND MONTH(day_start) Like N'%" + cboThang.Text + "%'";
-            if (txtNam.Text != "")
-                sql = sql + " AND YEAR(day_start) Like N'%" + txtNam.Text + "%'";
-
+            if ((cboThang.Text != "") && (cboQuy.Text != "") || (cboThang.Text != "") &&(txtNam.Text != "")|| (cboQuy.Text != "") && (txtNam.Text != "")|| (cboThang.Text != "") && (cboQuy.Text != "") && (txtNam.Text != ""))
+            {
+                MessageBox.Show("Chỉ chọn một kiện tìm kiếm!!!", "Yêu cầu ...", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Reset_Values();
+                return;
+            }
+            sql = "SELECT c.id_phong, sum(a.tongchiphi) AS Tong FROM BaoTri a JOIN CTBT b ON a.id_baotri=b.id_baotri JOIN MayTinh c ON c.id_may= b.id_may WHERE 1=1";
+            if (cboThang.SelectedIndex != -1)
+            {
+                cboQuy.Enabled = false;
+                txtNam.Enabled = false;
+                sql = sql + " AND (SELECT MONTH(a.day_start) AS CHAR) ='" + cboThang.Text + "' GROUP BY c.id_phong";
+            }
+            if (cboQuy.SelectedIndex != -1)
+            {
+                cboThang.Enabled = false;
+                txtNam.Enabled = false;
+                if (cboQuy.Text == "1")
+                {
+                    sql = sql + "AND (SELECT MONTH(a.day_start) AS CHAR) BETWEEN '1' AND '3' GROUP BY c.id_Phong";
+                }
+                if (cboQuy.Text == "2")
+                {
+                    sql = sql + "AND (SELECT MONTH(a.day_start) AS CHAR) BETWEEN '4' AND '6' GROUP BY c.id_Phong";
+                }
+                if (cboQuy.Text == "3")
+                {
+                    sql = sql + "AND (SELECT MONTH(a.day_start) AS CHAR) BETWEEN '7' AND '9' GROUP BY c.id_Phong";
+                }
+                if (cboQuy.Text == "4")
+                {
+                    sql = sql + "AND (SELECT MONTH(a.day_start) AS CHAR) BETWEEN '9' AND '12' GROUP BY c.id_Phong";
+                }
+            }
+            if (txtNam.Text.Length != 0)
+            {
+                cboThang.Enabled = false;
+                cboQuy.Enabled = false;
+                sql = sql + "AND (SELECT YEAR(a.day_start) AS CHAR)='" + txtNam.Text.Trim() + "' GROUP BY c.id_Phong";
+            }
             tbla = Functions.GetDataToTable(sql);
             if (tbla.Rows.Count == 0)
+            {
                 MessageBox.Show("Không có bản ghi thỏa mãn điều kiện!!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Reset_Values();
+                btnInbaocao.Enabled = false;
+                btnRefresh.Enabled = false;
+                btnBaocao.Enabled = true;
+            }
             else
+            {
                 MessageBox.Show("Có " + tbla.Rows.Count + " bản ghi thỏa mãn điều kiện!!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            DataGridView.DataSource = tbla;
-            Load_DataGridView();
-            btnBaocao.Enabled = false;
-            btnRefresh.Enabled = true;
-            btnInbaocao.Enabled = true;
+                DataGridView.DataSource = tbla;
+                Load_DataGridView();
+                cboQuy.Enabled = false;
+                cboThang.Enabled = false;
+                txtNam.Enabled = false;
+                btnBaocao.Enabled = false;
+                btnRefresh.Enabled = true;
+                btnInbaocao.Enabled = true;
+            }
         }
 
         private void btnRefresh_Click(object sender, EventArgs e)
@@ -71,7 +119,8 @@ namespace Quanly.Báo_cáo
             DataGridView.DataSource = null;
             btnBaocao.Enabled = true;
             btnInbaocao.Enabled = false;
-            btnRefresh.Enabled=false
+            btnRefresh.Enabled = false;
+            
         }
 
         private void btnDong_Click(object sender, EventArgs e)
