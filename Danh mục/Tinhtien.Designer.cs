64ba6f66435181a97f2eabc69e25b6e4820cb804
hungw2810 <hungnn2810@gmail.com>
2021-06-25 15:13:08 +0700

    form tinh tien(chua xong) form thue may(done)

--- Danh mục/Tinhtien.Designer.cs
+++ Danh mục/Tinhtien.Designer.cs
@@ -29,12 +29,87 @@ namespace Quanly.Danh_mục
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dtgv = new System.Windows.Forms.DataGridView();
+            this.btnThanhtoan = new System.Windows.Forms.Button();
+            this.lblTongtien = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgv
+            // 
+            this.dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv.Location = new System.Drawing.Point(22, 32);
+            this.dtgv.Name = "dtgv";
+            this.dtgv.Size = new System.Drawing.Size(332, 217);
+            this.dtgv.TabIndex = 0;
+            // 
+            // btnThanhtoan
+            // 
+            this.btnThanhtoan.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThanhtoan.Location = new System.Drawing.Point(35, 271);
+            this.btnThanhtoan.Name = "btnThanhtoan";
+            this.btnThanhtoan.Size = new System.Drawing.Size(89, 23);
+            this.btnThanhtoan.TabIndex = 1;
+            this.btnThanhtoan.Text = "Thanh toán";
+            this.btnThanhtoan.UseVisualStyleBackColor = true;
+            // 
+            // lblTongtien
+            // 
+            this.lblTongtien.AutoSize = true;
+            this.lblTongtien.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTongtien.ForeColor = System.Drawing.Color.Red;
+            this.lblTongtien.Location = new System.Drawing.Point(151, 272);
+            this.lblTongtien.Name = "lblTongtien";
+            this.lblTongtien.Size = new System.Drawing.Size(65, 19);
+            this.lblTongtien.TabIndex = 2;
+            this.lblTongtien.Text = "Tổng tiền";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 272);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(354, 253);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách máy thuê";
+            // 
+            // Tinhtien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Tinhtien";
+            this.ClientSize = new System.Drawing.Size(378, 315);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTongtien);
+            this.Controls.Add(this.btnThanhtoan);
+            this.Controls.Add(this.dtgv);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Tinhtien";
+            this.Text = "Tính tiền";
+            this.Load += new System.EventHandler(this.Tinhtien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dtgv;
+        private System.Windows.Forms.Button btnThanhtoan;
+        private System.Windows.Forms.Label lblTongtien;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- Danh mục/Tinhtien.cs
+++ Danh mục/Tinhtien.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
+using System.Data ;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
 
 namespace Quanly.Danh_mục
 {
     public partial class Tinhtien : Form
     {
+        DataTable tbla;
         public Tinhtien()
         {
             InitializeComponent();
+        }
+
+        private void Tinhtien_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Load_dtgv()
+        {
+           // string sql = "SELECT id_may,id_phong,TenKhach,GioVao,id_NhanVien FROM MayTinh WHERE ";
         }
     }
 }
