1bdf94a756237e79d2f6dd7819d44c0ed43669a9
thanhhvnh01 <duythanh.qn1@gmail.com>
2021-06-27 22:51:04 +0700

    bc tien thua (chua xong excel)

--- Báo cáo/Tienthue.Designer.cs
+++ Báo cáo/Tienthue.Designer.cs
@@ -29,6 +29,7 @@ namespace Quanly.Báo_cáo
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnTim = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -43,16 +44,19 @@ namespace Quanly.Báo_cáo
             this.label3 = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRP = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnTim
             // 
             this.btnTim.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTim.Location = new System.Drawing.Point(31, 207);
-            this.btnTim.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTim.Location = new System.Drawing.Point(17, 288);
+            this.btnTim.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnTim.Name = "btnTim";
-            this.btnTim.Size = new System.Drawing.Size(74, 25);
+            this.btnTim.Size = new System.Drawing.Size(99, 31);
             this.btnTim.TabIndex = 4;
             this.btnTim.Text = "Find";
             this.btnTim.UseVisualStyleBackColor = true;
@@ -63,10 +67,9 @@ namespace Quanly.Báo_cáo
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.SlateGray;
-            this.label1.Location = new System.Drawing.Point(18, 25);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(24, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.Size = new System.Drawing.Size(57, 21);
             this.label1.TabIndex = 5;
             this.label1.Text = "Tháng";
             // 
@@ -75,10 +78,9 @@ namespace Quanly.Báo_cáo
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.SlateGray;
-            this.label2.Location = new System.Drawing.Point(26, 105);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(35, 129);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.Size = new System.Drawing.Size(45, 21);
             this.label2.TabIndex = 5;
             this.label2.Text = "Năm";
             // 
@@ -99,10 +101,10 @@ namespace Quanly.Báo_cáo
             "10",
             "11",
             "12"});
-            this.cbxThang.Location = new System.Drawing.Point(76, 23);
-            this.cbxThang.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbxThang.Location = new System.Drawing.Point(101, 28);
+            this.cbxThang.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbxThang.Name = "cbxThang";
-            this.cbxThang.Size = new System.Drawing.Size(95, 23);
+            this.cbxThang.Size = new System.Drawing.Size(125, 27);
             this.cbxThang.TabIndex = 9;
             this.cbxThang.SelectedIndexChanged += new System.EventHandler(this.cbxThang_SelectedIndexChanged);
             // 
@@ -114,10 +116,10 @@ namespace Quanly.Báo_cáo
             "2020",
             "2021",
             "2022"});
-            this.cbxNam.Location = new System.Drawing.Point(76, 103);
-            this.cbxNam.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbxNam.Location = new System.Drawing.Point(101, 127);
+            this.cbxNam.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbxNam.Name = "cbxNam";
-            this.cbxNam.Size = new System.Drawing.Size(95, 23);
+            this.cbxNam.Size = new System.Drawing.Size(125, 27);
             this.cbxNam.TabIndex = 9;
             // 
             // radZoneA
@@ -125,10 +127,10 @@ namespace Quanly.Báo_cáo
             this.radZoneA.AutoSize = true;
             this.radZoneA.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radZoneA.ForeColor = System.Drawing.Color.SlateGray;
-            this.radZoneA.Location = new System.Drawing.Point(220, 25);
-            this.radZoneA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radZoneA.Location = new System.Drawing.Point(293, 31);
+            this.radZoneA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radZoneA.Name = "radZoneA";
-            this.radZoneA.Size = new System.Drawing.Size(70, 21);
+            this.radZoneA.Size = new System.Drawing.Size(86, 25);
             this.radZoneA.TabIndex = 10;
             this.radZoneA.TabStop = true;
             this.radZoneA.Text = "Zone A";
@@ -139,10 +141,10 @@ namespace Quanly.Báo_cáo
             this.radZoneB.AutoSize = true;
             this.radZoneB.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radZoneB.ForeColor = System.Drawing.Color.SlateGray;
-            this.radZoneB.Location = new System.Drawing.Point(220, 64);
-            this.radZoneB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radZoneB.Location = new System.Drawing.Point(293, 79);
+            this.radZoneB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radZoneB.Name = "radZoneB";
-            this.radZoneB.Size = new System.Drawing.Size(70, 21);
+            this.radZoneB.Size = new System.Drawing.Size(86, 25);
             this.radZoneB.TabIndex = 10;
             this.radZoneB.TabStop = true;
             this.radZoneB.Text = "Zone B";
@@ -153,10 +155,10 @@ namespace Quanly.Báo_cáo
             this.radZoneC.AutoSize = true;
             this.radZoneC.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radZoneC.ForeColor = System.Drawing.Color.SlateGray;
-            this.radZoneC.Location = new System.Drawing.Point(219, 105);
-            this.radZoneC.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radZoneC.Location = new System.Drawing.Point(292, 129);
+            this.radZoneC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radZoneC.Name = "radZoneC";
-            this.radZoneC.Size = new System.Drawing.Size(70, 21);
+            this.radZoneC.Size = new System.Drawing.Size(87, 25);
             this.radZoneC.TabIndex = 10;
             this.radZoneC.TabStop = true;
             this.radZoneC.Text = "Zone C";
@@ -171,10 +173,10 @@ namespace Quanly.Báo_cáo
             "2",
             "3",
             "4"});
-            this.cboQuy.Location = new System.Drawing.Point(76, 62);
-            this.cboQuy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboQuy.Location = new System.Drawing.Point(101, 76);
+            this.cboQuy.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cboQuy.Name = "cboQuy";
-            this.cboQuy.Size = new System.Drawing.Size(95, 23);
+            this.cboQuy.Size = new System.Drawing.Size(125, 27);
             this.cboQuy.TabIndex = 11;
             this.cboQuy.SelectedIndexChanged += new System.EventHandler(this.cboQuy_SelectedIndexChanged);
             // 
@@ -183,20 +185,19 @@ namespace Quanly.Báo_cáo
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.SlateGray;
-            this.label4.Location = new System.Drawing.Point(30, 64);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(40, 79);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.Size = new System.Drawing.Size(42, 21);
             this.label4.TabIndex = 12;
             this.label4.Text = "Quý";
             // 
             // btnrefresh
             // 
             this.btnrefresh.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnrefresh.Location = new System.Drawing.Point(128, 207);
-            this.btnrefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnrefresh.Location = new System.Drawing.Point(122, 288);
+            this.btnrefresh.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnrefresh.Name = "btnrefresh";
-            this.btnrefresh.Size = new System.Drawing.Size(74, 25);
+            this.btnrefresh.Size = new System.Drawing.Size(99, 31);
             this.btnrefresh.TabIndex = 13;
             this.btnrefresh.Text = "Refresh";
             this.btnrefresh.UseVisualStyleBackColor = true;
@@ -207,19 +208,19 @@ namespace Quanly.Báo_cáo
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(84, 174);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(76, 251);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.Size = new System.Drawing.Size(86, 21);
             this.label3.TabIndex = 14;
             this.label3.Text = "Tổng tiền ";
             // 
             // btnCancel
             // 
             this.btnCancel.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(225, 209);
+            this.btnCancel.Location = new System.Drawing.Point(228, 288);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(100, 31);
             this.btnCancel.TabIndex = 15;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -238,24 +239,52 @@ namespace Quanly.Báo_cáo
             this.groupBox1.Controls.Add(this.radZoneB);
             this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.groupBox1.Location = new System.Drawing.Point(11, 12);
+            this.groupBox1.Location = new System.Drawing.Point(17, 52);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(308, 150);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(411, 185);
             this.groupBox1.TabIndex = 16;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "THÔNG TIN";
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(290, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 17;
+            // 
+            // btnRP
+            // 
+            this.btnRP.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRP.Location = new System.Drawing.Point(336, 288);
+            this.btnRP.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRP.Name = "btnRP";
+            this.btnRP.Size = new System.Drawing.Size(100, 31);
+            this.btnRP.TabIndex = 15;
+            this.btnRP.Text = "Export";
+            this.btnRP.UseVisualStyleBackColor = true;
+            this.btnRP.Click += new System.EventHandler(this.btn_RP_Click);
+            // 
             // Tienthue
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 243);
+            this.ClientSize = new System.Drawing.Size(451, 338);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnRP);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnrefresh);
             this.Controls.Add(this.btnTim);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Tienthue";
             this.Text = "Báo cáo tiền thuê";
             this.Load += new System.EventHandler(this.Tienthue_Load);
@@ -281,5 +310,8 @@ namespace Quanly.Báo_cáo
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRP;
     }
 }
--- Báo cáo/Tienthue.cs
+++ Báo cáo/Tienthue.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using COMExcel = Microsoft.Office.Interop.Excel;
 
 namespace Quanly.Báo_cáo
 {
@@ -104,6 +105,126 @@ namespace Quanly.Báo_cáo
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+     
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            label5.Text = time.ToString("HH:mm:ss");
+        }
+
+        private void btn_RP_Click(object sender, EventArgs e)
+        {
+            // Khởi động chương trình Excel
+
+
+            COMExcel.Application exApp = new COMExcel.Application();
+            COMExcel.Workbook exBook; //Trong 1 chương trình Excel có nhiều Workbook
+            COMExcel.Worksheet exSheet; //Trong 1 Workbook có nhiều Worksheet
+            COMExcel.Range exRange;
+            string sql;
+            int hang = 0, cot = 0;
+            DataTable ThongtinNBT, ThongtinBaotri;
+            exBook = exApp.Workbooks.Add(COMExcel.XlWBATemplate.xlWBATWorksheet);
+            exSheet = exBook.Worksheets[1];
+            // Định dạng chung
+            exRange = exSheet.Cells[1, 1];
+            exRange.Range["A1:B3"].Font.Size = 10;
+            exRange.Range["A1:B3"].Font.Name = "Times new roman";
+            exRange.Range["A1:B3"].Font.Bold = true;
+            exRange.Range["A1:B3"].Font.ColorIndex = 5; //Màu xanh da trời
+            exRange.Range["A1:A1"].ColumnWidth = 7;
+            exRange.Range["B1:B1"].ColumnWidth = 15;
+            exRange.Range["A1:B1"].MergeCells = true;
+            exRange.Range["A1:B1"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["A1:B1"].Value = "Cửa hàng Internet";
+
+            exRange.Range["A2:B2"].MergeCells = true;
+            exRange.Range["A2:B2"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["A2:B2"].Value = "Hà Nội";
+
+            exRange.Range["A3:B3"].MergeCells = true;
+            exRange.Range["A3:B3"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["A3:B3"].Value = "Điện thoại: (04)88889999";
+
+
+            exRange.Range["C2:E2"].Font.Size = 16;
+            exRange.Range["C2:E2"].Font.Name = "Times new roman";
+            exRange.Range["C2:E2"].Font.Bold = true;
+            exRange.Range["C2:E2"].Font.ColorIndex = 3; //Màu đỏ
+            exRange.Range["C2:E2"].MergeCells = true;
+            exRange.Range["C2:E2"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["C2:E2"].Value = "BÁO CÁO CHI PHÍ";
+            // Biểu diễn thông tin chung của Báo cáo chi phí
+            sql = "SELECT a.";
+            ThongtinNBT = Functions.GetDataToTable(sql);
+            exRange.Range["B6:C9"].Font.Size = 12;
+            exRange.Range["B6:C9"].Font.Name = "Times new roman";
+            exRange.Range["B6:B6"].Value = "Mã nhà bảo trì:";
+            exRange.Range["C6:E6"].MergeCells = true;
+            exRange.Range["C6:E6"].Value = ThongtinNBT.Rows[0][0].ToString();
+            exRange.Range["B7:B7"].Value = "Tên nhà bảo trì:";
+            exRange.Range["C7:E7"].MergeCells = true;
+            exRange.Range["C7:E7"].Value = ThongtinNBT.Rows[0][1].ToString();
+            exRange.Range["B8:B8"].Value = "Địa chỉ:";
+            exRange.Range["C8:J8"].MergeCells = true;
+            exRange.Range["C8:E8"].Value = ThongtinNBT.Rows[0][2].ToString();
+            exRange.Range["B9:B9"].Value = "Điện thoại:";
+            exRange.Range["C9:E9"].MergeCells = true;
+            exRange.Range["C9:E9"].Value = ThongtinNBT.Rows[0][3].ToString();
+            //Lấy thông tin bảo trì
+            sql = "SELECT c.id_phong, sum(a.tongchiphi) AS Tong FROM BaoTri a JOIN CTBT b ON a.id_baotri=b.id_baotri JOIN MayTinh c ON c.id_may= b.id_may WHERE 1=1";
+            if (cboThang.SelectedIndex != -1)
+            {
+                cboQuy.Enabled = false;
+                txtNam.Enabled = false;
+                sql = sql + " AND (SELECT MONTH(a.day_start) AS CHAR) ='" + cboThang.Text + "' GROUP BY c.id_phong";
+            }
+            if (cboQuy.SelectedIndex != -1)
+            {
+                cboThang.Enabled = false;
+                txtNam.Enabled = false;
+                if (cboQuy.Text == "1")
+                {
+                    sql = sql + "AND (SELECT MONTH(a.day_start) AS CHAR) BETWEEN '1' AND '3' GROUP BY c.id_Phong";
+                }
+                if (cboQuy.Text == "2")
+                {
+                    sql = sql + "AND (SELECT MONTH(a.day_start) AS CHAR) BETWEEN '4' AND '6' GROUP BY c.id_Phong";
+                }
+                if (cboQuy.Text == "3")
+                {
+                    sql = sql + "AND (SELECT MONTH(a.day_start) AS CHAR) BETWEEN '7' AND '9' GROUP BY c.id_Phong";
+                }
+                if (cboQuy.Text == "4")
+                {
+                    sql = sql + "AND (SELECT MONTH(a.day_start) AS CHAR) BETWEEN '9' AND '12' GROUP BY c.id_Phong";
+                }
+            }
+            if (txtNam.Text.Length != 0)
+            {
+                cboThang.Enabled = false;
+                cboQuy.Enabled = false;
+                sql = sql + "AND (SELECT YEAR(a.day_start) AS CHAR)='" + txtNam.Text.Trim() + "' GROUP BY c.id_Phong";
+            }
+            ThongtinBaotri = Functions.GetDataToTable(sql);
+            //Tạo dòng tiêu đề bảng
+            exRange.Range["A11:F11"].Font.Bold = true;
+            exRange.Range["A11:F11"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["A11:F11"].ColumnWidth = 15;
+            exRange.Range["A11:A11"].Value = "Mã phòng";
+            exRange.Range["B11:B11"].Value = "Tổng tiền";
+            for (hang = 0; hang <= ThongtinBaotri.Rows.Count - 1; hang++)
+            {
+                for (cot = 0; cot <= ThongtinBaotri.Columns.Count - 1; cot++)
+                    //Điền thông tin hàng từ cột thứ 1, dòng 12
+                    exSheet.Cells[cot + 1][hang + 12] = ThongtinBaotri.Rows[hang][cot].ToString();
+            }
+
+            exSheet.Name = "Báo Cáo Chi Phí";
+            exApp.Visible = true;
+
         }
     }
 }
